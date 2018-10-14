a43cf1a9076630efe5081082ade88fe875262f85
unknown <archieinet@gmail.com>
2018-10-13 19:16:19 -0500

    destructure picklist

--- force-app/main/default/aura/requestForService/requestForServiceHelper.js
+++ force-app/main/default/aura/requestForService/requestForServiceHelper.js
@@ -1,5 +1,12 @@
 ({
-    helperMethod : function() {
-
+    init: function (cmp) {
+        let action = cmp.get("c.GetCSPickList");
+        action.setCallback(this, function(resp){
+            let state = resp.getState();
+            if (cmp.isValid() && state === "SUCCESS") {
+                let {plReqType, plAcctType, plFundype, plPriorityType} = resp.getReturnValue();
+             }
+        });
+      $A.enqueueAction(action);
     }
 })
